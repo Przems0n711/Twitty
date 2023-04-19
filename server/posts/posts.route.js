fe0771af3209419198b8c563f27befb879a841d8
Rexoo <herricux99@gmail.com>
2023-04-19 12:12:53 +0200

    Twitty Version 1.6.1
    Deleted old mongoDB database :cry:
    
    Imported new postgreSQL database :thumbsup:
    
    Connected database with docker :whale:
    
    Fixed bugs :white_check_mark:

--- server/posts/posts.route.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const postsService = require('./service');
-
-router.get('/api/posts', postsService.posts);
-
-module.exports = router;
--- server/tokens/tokens.route.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userService = require('./service/tokens');
-
-router.post('/api/token/verify', userService.verifyToken);
-module.exports = router;
